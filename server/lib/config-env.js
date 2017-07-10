0e0929841198312317831765072885b828ed4f1e
or4nga <patrick.tang1086@gmail.com>
2017-07-10 17:57:06 -0500

    Definitely not an api key

---
+++ server/lib/config-env.js
@@ -0,0 +1,7 @@
+exports.cloudinary = {
+  NAME: 'jamesquillin',
+  API_KEY: '176385718454749',
+  API_SECRET: 'dpB_FFQMH0wRvIMSsQQ6HhA2ohM',
+};
+
+exports.mongooseUri = 'mongodb://localhost/example';
